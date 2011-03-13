22bb67a3c73d6cffc161185eb78e1cd97c39bb3e
Weibin Yao(姚伟斌) <yaoweibin@gmail.com>
2011-03-13 20:29:35 +0800

    fix style

--- encoding.rb
+++ encoding.rb
@@ -1,16 +1,19 @@
+
 require'iconv'  
+
 
 class String  
 
   def to_gbk  
-    Iconv.iconv("GBK//IGNORE","UTF-8//IGNORE",self).to_s  
+    Iconv.iconv("GBK//IGNORE//TRANSLIT", "UTF-8//IGNORE//TRANSLIT", self).to_s  
   end  
 
   def to_utf8  
-    Iconv.iconv("UTF-8//IGNORE","GB2312//IGNORE",self).to_s  
+    Iconv.iconv("UTF-8//IGNORE//TRANSLIT", "GBK//IGNORE//TRANSLIT", self).to_s  
   end  
 
 end  
+
 
 s = "\xC4\xE3\xBA\xC3"
 
